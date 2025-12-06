b77ad5dd07a666d0df87aa4968c8094e0fe6aee6
Moh Aamir <mohaamir0706@gmail.com>
2025-12-06 18:15:38 +0530

    Implement the javaScript logic to check winner makeMove function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    let board = Array(3)
+                .fill(null)
+                .map(()=>Array(3).fill(null));
     const cells = document.querySelectorAll('[data-cell]');
     let currentPlayer = 'X';
     let isGameOVer = false;
+    let isWinner = null;
     const isWinningPosition = [
         [0,1,2],
             [3,4,5],
@@ -12,13 +16,42 @@ document.addEventListener("DOMContentLoaded",()=>{
             [0,4,8],
             [2,4,6]
     ];
+    function checkWinner(row,col){
+        console.log("check winner active",currentPlayer);
+        let player = board[row][col];
+        for(const [a,b,c] of isWinningPosition){
+           if(board[Math.floor(a/3)][a%3] == player &&
+            board[Math.floor(b/3)][b%3] == player &&
+            board[Math.floor(c/3)][c%3] == player
+           )
+        return true;
+        }
+    }
 
-    console.log(cells);
-    cells.forEach(element => {
-        element.addEventListener('click',(e)=>{
-            console.log(e.target.dataset.index);
+    function makeMove(e){
+        console.log(e.target);
+        idx = e.target.dataset.index;
+        let row =Math.floor( idx/3);
+        let col = Math.floor(idx%3);
+        console.log(row,col);
+        if(isGameOVer || board[row][col] !== null ) return;
+        board[row][col] = currentPlayer;
+        e.target.textContent = currentPlayer;
+        console.log(currentPlayer);
+        if(checkWinner(row,col)){
+            alert("Winner is "+currentPlayer);
+            isGameOVer = true;
+            isWinner = currentPlayer;
+        }else{
+            currentPlayer = currentPlayer === 'X'?'O':'X';
+        }
 
+    }
+
+    function initateGame(){
+        cells.forEach(c=>{
+            c.addEventListener('click',makeMove)
         })
-        
-    });
+    }
+    initateGame();
 })
