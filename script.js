ca14d0d0bb5cc56401ed5c64c0ac7674b18f3b04
Moh Aamir <mohaamir0706@gmail.com>
2025-12-06 23:03:55 +0530

    Complete the logic of tic tac toe using js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",function(){
     let board = Array(3)
                 .fill(null)
                 .map(()=>Array(3).fill(null));
     const cells = document.querySelectorAll('[data-cell]');
+    const pl = document.querySelector('[data-status]');
+    const reset = document.querySelector('[data-reset]');
+    const bb = document.querySelector('[data-board]');
+    const start = document.querySelector('[data-start]');
     let currentPlayer = 'X';
     let isGameOVer = false;
     let isWinner = null;
@@ -16,8 +20,21 @@ document.addEventListener("DOMContentLoaded",()=>{
             [0,4,8],
             [2,4,6]
     ];
+    function resetHtml(){
+         board = Array(3)
+                .fill(null)
+                .map(()=>Array(3).fill(null));
+        cells.forEach(c=>{
+            c.textContent = '';
+        })
+        // start.setAttribute('enabled','');
+        // start.addEventListener('click',initateGame);
+        initateGame();
+        
+    }
+    reset.addEventListener('click',resetHtml);
     function checkWinner(row,col){
-        console.log("check winner active",currentPlayer);
+        // console.log("check winner active",currentPlayer);
         let player = board[row][col];
         for(const [a,b,c] of isWinningPosition){
            if(board[Math.floor(a/3)][a%3] == player &&
@@ -27,9 +44,15 @@ document.addEventListener("DOMContentLoaded",()=>{
         return true;
         }
     }
-
+    function isDraw(){if(!isGameOVer) return false;
+        for(let row =0;row<3;row++){
+            for(let col =0;col<3;col++){
+                if(this.board[row][col]===null) return false;
+            }
+        }
+        return true;}
     function makeMove(e){
-        console.log(e.target);
+        // console.log(e.target);
         idx = e.target.dataset.index;
         let row =Math.floor( idx/3);
         let col = Math.floor(idx%3);
@@ -37,21 +60,32 @@ document.addEventListener("DOMContentLoaded",()=>{
         if(isGameOVer || board[row][col] !== null ) return;
         board[row][col] = currentPlayer;
         e.target.textContent = currentPlayer;
-        console.log(currentPlayer);
+        // console.log(currentPlayer);
         if(checkWinner(row,col)){
-            alert("Winner is "+currentPlayer);
             isGameOVer = true;
             isWinner = currentPlayer;
+            alert("Winner is "+currentPlayer);
+            currentPlayer = 'X';
+            // initateGame();
+        }else if(isDraw()){
+            alert("Game is Draw !! Please Click Reset to Play Again");
+            // initateGame();
         }else{
             currentPlayer = currentPlayer === 'X'?'O':'X';
+            pl.textContent = `Player ${currentPlayer}`;
         }
 
     }
 
+    // start.addEventListener('click',initateGame);
+    
+
     function initateGame(){
+        isGameOVer = false;
         cells.forEach(c=>{
             c.addEventListener('click',makeMove)
         })
+        // start.setAttribute('disabled','');
     }
     initateGame();
-})
+});
